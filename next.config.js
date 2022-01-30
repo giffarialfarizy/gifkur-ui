e2a78ac0513ad6bc2fe2893b83e5dd99c5a210e6
Giffari Alfarizy <giffarialfarizy@gmail.com>
2022-01-30 11:12:59 +0700

    ➕ Sass

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: true,
+};
+
+// module.exports = nextConfig
+
+module.exports = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+	nextConfig,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
 	return (
-		<h1 className='p-4 pt-2 text-3xl font-bold text-blue-900 underline'>
-			Hello world!
-		</h1>
+		<main>
+			<h1 className='p-4 pt-2 text-3xl font-bold text-blue-900 underline'>
+				Hello world!
+			</h1>
+			<p className='testing'>Test borneo red</p>
+		</main>
 	);
 };
 
