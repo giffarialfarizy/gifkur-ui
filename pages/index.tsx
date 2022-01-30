62b29848c3399d78d30d86a623cf6eb7b77f768d
Giffari Alfarizy <giffarialfarizy@gmail.com>
2022-01-30 12:42:13 +0700

    🔥 Remove unnecessary testing code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
 	return (
-		<main>
+		<main className='h-screen bg-slate-400'>
 			<h1 className='p-4 pt-2 text-3xl font-bold text-blue-900 underline'>
 				Hello world!
 			</h1>
-			<p className='testing'>Test borneo red</p>
 		</main>
 	);
 };
